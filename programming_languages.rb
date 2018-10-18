7086ca79903cfcfeac129f45ced79797c9abe51a
anik1606 <anik1606@gmail.com>
2018-10-18 23:45:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, lang_hash|
+    lang_hash.each do |lang, type_hash|
+        
+    type_hash[:style] ||= []
+    type_hash[:style] << style
+   if new_hash[lang]
+     type_hash[:style].concat(type_hash[:style])
+   else 
+     new_hash[lang]
+   end
+  end
+  end
+
+  new_hash
 end
